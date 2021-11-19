c15ae76f787e93dfc02e717345de40cb5c58cfbd
Mojashi <akihiro1001@gmail.com>
2021-11-19 22:36:38 +0900

    test update

--- main.user.js
+++ main.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BETTER_T2SCHOLA
-// @version      0.1
+// @version      0.1.1
 // @description  T2SCHOLA is kasu
 // @author       mojashi
 // @match        https://t2schola.titech.ac.jp/*
