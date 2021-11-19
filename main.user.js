1607d4398968247f2efb2c51f42b949569d91ea3
Mojashi <akihiro1001@gmail.com>
2021-11-20 00:08:50 +0900

    impl OCW integration

--- main.user.js
+++ main.user.js
@@ -1,24 +1,50 @@
 // ==UserScript==
 // @name         BETTER_T2SCHOLA
-// @version      0.1.1
+// @version      0.2
 // @description  T2SCHOLA is kasu
 // @author       mojashi
 // @match        https://t2schola.titech.ac.jp/*
-// @grant        none
-// @updateURL	https://github.com/Mojashi/better-t2schola/blob/master/main.js
+// @grant        GM.xmlHttpRequest
+// @updateURL    https://github.com/Mojashi/better-t2schola/blob/master/main.js
 // ==/UserScript==
 
-(async function() {
-    'use strict';
+function GMfetch(method, url) {
+    return new Promise(function (resolve, reject) {
+        GM.xmlHttpRequest({
+            method, url,
+            onload:(response)=>{
+                if (response.status >= 200 && response.status < 300) {
+                    resolve(response.responseText);
+                } else {
+                    reject({
+                        status: response.status,
+                        statusText: response.statusText
+                    });
+                }
+            },
+            onerror:(response)=>{
+                reject({
+                    status: response.status,
+                    statusText: response.statusText
+                });
+            },
+        });
+    });
+}
+
+async function fetchHTML(url) {
+    var parser = new DOMParser();
+    
+    return parser.parseFromString(await GMfetch("GET", url), 'text/html');
+}
+
+async function fixMyCourseTitle(){
     const nodes = document.querySelectorAll('a[data-parent-key=mycourses]')
     async function getName(url){
         var parser = new DOMParser();
         const cached = localStorage.getItem("title_" + url)
         if(!cached) {
-            const title = await fetch(url).then(res=>res.text()).then(t=>{
-                const doc = parser.parseFromString(t, 'text/html');
-                return doc.querySelectorAll(".page-header-headings")[0].innerText
-            })
+            const title = await fetchHTML(url).querySelectorAll(".page-header-headings")[0].innerText
             localStorage.setItem("title_" + url, title)
         }
         return localStorage.getItem("title_" + url)
@@ -26,4 +52,56 @@
     for(var i = 0; nodes.length > i; i++){
         nodes[i].querySelector(".media-body").innerText = await getName(nodes[i].getAttribute('href'))
     }
+}
+
+async function OCWIntegration(){
+    if(location.href !== 'https://t2schola.titech.ac.jp/') return
+
+    const h = await fetchHTML('https://secure.ocw.titech.ac.jp/ocwi/index.php')
+    var nodes = Array(...h.querySelectorAll('#notPresented tr'))
+    if(nodes.length > 0){ 
+        nodes = nodes.slice(1)
+    }
+    
+    const styleElem = document.createElement("style")
+    styleElem.innerHTML = `
+    table.ocw-assignments {
+        width: 100%;
+        border: 1px solid #ccc;
+        border-spacing: 0;
+        background: white;
+    }
+    .ocw-assignments td {
+        padding: 5px;
+        border-right: solid 1px #ccc;
+        border-bottom: solid 1px #ccc;
+    }
+    .ocw-assignments th {
+        color: #ffffff;
+        background-color: #007cc1;
+        padding: 5px;
+    }
+    `
+    const table = document.createElement("table")
+    table.setAttribute("class", "ocw-assignments")
+    const tbody = document.createElement("tbody")
+    tbody.innerHTML = "<tr><th>課題締切日</th><th>講義名</th><th>課題タイトル</th></tr>"
+    nodes.forEach(node=>{
+        node.querySelectorAll("a").forEach(a=>{
+            url = new URL(a.href)
+            a.href = 'https://secure.ocw.titech.ac.jp/ocwi/index.php'+ url.search
+        })
+        tbody.append(node)
+    })
+    table.appendChild(tbody)
+    const mainDiv = document.querySelector("div[role=main]")
+    mainDiv.prepend(table)
+    mainDiv.prepend(styleElem)
+}
+
+(async function() {
+    'use strict';
+
+    await fixMyCourseTitle()
+    await OCWIntegration()
 })();
